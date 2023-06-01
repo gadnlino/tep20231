29ec00a5310e27a823eac06c19752fbb1e4253a4
gadnlino <guiavenas@gmail.com>
2023-06-01 09:02:44 -0300

    adicionando tentativas

--- src/aula6/euclid_problem/main.py
+++ src/aula6/euclid_problem/main.py
@@ -1,7 +1,9 @@
 #https://www.baeldung.com/java-least-common-multiple#:~:text=Using%20the%20Euclidean%20Algorithm,-There's%20an%20interesting&text=As%20stated%2C%20gcd(a%2C,gcd(a%2Cb).
+#https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%2012/1155_English.htm
+
+import math
 
 def gcde(a, b, d, x, y):
-    print(a, b, d, x, y)
     if (b == 0):
         d = a
         x = 1
@@ -12,7 +14,7 @@ def gcde(a, b, d, x, y):
     _, _, d, x, y = gcde(b, a % b, d, x, y)
 
     s = y
-    y = x - (a / b) * y
+    y = x - math.floor((a / b)) * y
     x = s
 
     return a, b, d, x, y
@@ -26,6 +28,6 @@ while True:
 
         a, b, d, x, y = gcde(a, b, a, 0, 0)
 
-        print(min(x, y), max(x, y))
+        print(x, y, d)
     except EOFError:
         break
